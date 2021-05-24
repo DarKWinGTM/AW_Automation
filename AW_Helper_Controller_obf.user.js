85eaf1a1ba18d6d5ac17213931a45cba3f095d5e
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-05-24 09:47:36 +0700

    AW_Helper_Controller_obf.user.js
    Added New Script

---
+++ AW_Helper_Controller_obf.user.js
@@ -0,0 +1,13 @@
+// @name          AW_Helper_Controller
+// @namespace     Violentmonkey Scripts
+// @include       *.wax.*
+// @include       *awhelper.*
+// @include       *localhost*
+// @grant		  unsafeWindow
+// @grant		  GM_xmlhttpRequest
+// @version       1.1
+// @author        -
+// @updateURL     https://
+// @downloadURL   https://
+// @description   5/24/2021, 3:27:02 PM
+// ==/UserScript==
