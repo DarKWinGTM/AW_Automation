91a015e901872132a98fc4a543ddb9701f8c7819
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-05-26 13:11:07 +0700

    AW_Helper_Controller_obf-Global.user.js
    Create

---
+++ AW_Helper_Controller_obf-Global.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name          AW_Helper_Controller
+// @namespace     Violentmonkey Scripts
+// @include       *.wax.*
+// @include       *awhelper.*
+// @include       *localhost*
+// @grant         unsafeWindow
+// @grant         GM_xmlhttpRequest
+// @version       2.1-Global
+// @author        -
+// @updateURL     
+// @downloadURL   
+// @description   5/26/2021, 02:00:00 PM
+// ==/UserScript==
