b6a0455f74af072be4a2c60fa0df61d1d408a0b6
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-06-01 22:29:24 +0700

    2.51-Global
    2.51-Global

--- AW_Helper_Controller_obf-Global.user.js
+++ AW_Helper_Controller_obf-Global.user.js
@@ -2,11 +2,13 @@
 // @name          AW_Helper_Controller
 // @namespace     Violentmonkey Scripts
 // @include       *.wax.*
-// @include       *awhelper.*
-// @include       *localhost*
+// @include       DISABLE-*awhelper.*
+// @include       *.alien*script.*
+// @include       DISABLE-*.pusanstudio.*
+// @include       DISABLE-*localhost*
 // @grant		  unsafeWindow
 // @grant		  GM_xmlhttpRequest
-// @version       2.5-Global
+// @version       2.51-Global
 // @author        Thanks for YoYoisMe
 // @updateURL     https://github.com/DarKWinGTM/AW_Automation/raw/main/AW_Helper_Controller_obf-Global.user.js
 // @downloadURL   https://github.com/DarKWinGTM/AW_Automation/raw/main/AW_Helper_Controller_obf-Global.user.js
