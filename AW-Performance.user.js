f2b2852bd63db93d2813b91fd34a7525a9858a6b
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-06-20 03:27:21 +0700

    AW-Performance.user.js
    AW-Performance.user.js

---
+++ AW-Performance.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        AW-Performance
+// @version     0.148
+// @description AW-Performance
+// @match       *://all-access.wax.io/cloud-wallet/signing*
+// @author      Author
+// @grant       window.focus
+// @grant       window.close
+// @grant       unsafeWindow
+// @run-at      document-start
+// @downloadURL https://github.com/DarKWinGTM/AW_Automation/raw/main/AW-Performance.user.js
+// @updateURL   https://github.com/DarKWinGTM/AW_Automation/raw/main/AW-Performance.user.js
+// ==/UserScript==
+
+window.close        = function (){ return }; 
+unsafeWindow.close  = function (){ return }; 
+self.close          = function (){ return }; 
